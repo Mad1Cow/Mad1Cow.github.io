34b513edcbf6ad1309229592e329c980077375af
Mad1Cow <70408171+Mad1Cow@users.noreply.github.com>
2020-11-06 17:52:36 +0000

    Add files via upload

---
+++ RandomNameGenerator.js
@@ -0,0 +1,18 @@
+let elfMNameArray = [
+    'Adran', 'Aelar', 'Aramil', 'Arannis', 'Aust', 'Beiro', 'Berrian', 'Carric', 'Enialis',
+    'Erdan', 'Erevan', 'Galinndan', 'Hadarai', 'Heian', 'Himo', 'Immeral', 'Ivellios', 'Laucian',
+    'Mindartis', 'Paelias', 'Peren', 'Quarion', 'Riardon', 'Rolen', 'Soveliss', 'Thamior',
+    'Tharivol', 'Theren', 'Varis'
+];
+let elfFNameArray = [
+    'Adrie', 'Althaea', 'Anastrianna', 'Andraste', 'Antinua', 'Bethrynna', 'Birel', 'Caelynn',
+    'Drusilia', 'Enna', 'Felosial', 'Ielenia', 'Jelenneth', 'Keyleth', 'Leshanna', 'Lia',
+    'Meriele', 'Mialee', 'Naivara', 'Quelenna', 'Quillathe', 'Sariel', 'Shanairra', 'Shava', 'Silaqui',
+    'Theirastra', 'Thia', 'Vadania', 'Valanthe', 'Xanaphia'
+]
+
+let randomNameM = parseInt(Math.random() * elfMNameArray.length);
+let randomNameF = parseInt(Math.random() * elfFNameArray.length);
+
+console.log('Male Elf: ' + elfMNameArray[randomNameM]);
+console.log('Female Elf: ' + elfFNameArray[randomNameF]);
